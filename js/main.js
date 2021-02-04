f0a8b1370dbec13f77c5cbecd69f561fa78625d3
nstsautchenkova <nst.sautchenkova@gmail.com>
2021-02-04 19:21:05 +0300

    Написание функций

--- js/main.js
+++ js/main.js
@@ -6,17 +6,16 @@
  min < max
 */
 
-function randomInteger(min, max) {
-  if ((min < 0) || (max < 0)){
+function getRandomInteger(min, max) {
+  if ((min < 0) || (max < 0)) {
     return new Error('Диапазон может быть больше или равен нулю')
-  }else if (min >= max){
+  } else if (min >= max) {
     return new Error('Начальное значение диапазона не должно быть больше или равным конечному');
-  }else{
-    let rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    return Math.round(rand);
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
-alert(randomInteger(1, 6));
+alert(getRandomInteger(1, 6));
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 /*
@@ -26,16 +25,15 @@ alert(randomInteger(1, 6));
   min < max
   flag < 0
 */
-function randomDecimal(min, max, flag) {
-  if ((min < 0) || (max < 0)){
+function getRandomDecimal(min, max, decimalDigits) {
+  if ((min < 0) || (max < 0)) {
     return new Error('Диапазон может быть больше или равен нулю');
-  }else if (flag < 0){
+  } else if (decimalDigits < 0) {
     return new Error('Колличесво чисел после запятой может быть больше или равен нулю');
-  }else if (min >= max){
+  } else if (min >= max) {
     return new Error('Начальное значение диапазона не должно быть больше или равным конечному');
-  }else{
-    let rand = Math.random() * (max - min) + min;
-    return rand.toFixed(flag);
+  } else {
+    return (Math.random() * (max - min) + min).toFixed(decimalDigits);
   }
 }
-alert(randomDecimal(1, 9, 2));
+alert(getRandomDecimal(1, 9, 2));
