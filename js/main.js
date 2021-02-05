5a6f39757fbd4bf3d35dee376cae9cd9a93b6bf7
nstsautchenkova <nst.sautchenkova@gmail.com>
2021-02-05 17:46:55 +0300

    Начало работы в файле main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,30 @@
 // Функция, возвращающая случайное число из переданного диапазона включительно min ... max
-/*
- имя_функции(от, до);
- Результат: целое число из диапазона "от...до".
- Диапазон больше или равен 0.
- min < max
-*/
 
-function randomInteger(min, max) {
-  if ((min < 0) || (max < 0)){
+function getRandomInteger(min, max) {
+  if ((min < 0) || (max < 0)) {
     return new Error('Диапазон может быть больше или равен нулю')
-  }else if (min >= max){
+  } else if (min >= max) {
     return new Error('Начальное значение диапазона не должно быть больше или равным конечному');
-  }else{
-    let rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    return Math.round(rand);
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
-alert(randomInteger(1, 6));
+getRandomInteger(1,9);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-/*
-  имя_функции(от, до, количество_знаков_после_запятой);
-  Результат: целое число из диапазона "от...до"
-  Диапазон больше или равен 0
-  min < max
-  flag < 0
-*/
-function randomDecimal(min, max, flag) {
-  if ((min < 0) || (max < 0)){
+
+function getRandomDecimal(min, max, decimalDigits) {
+  if ((min < 0) || (max < 0)) {
     return new Error('Диапазон может быть больше или равен нулю');
-  }else if (flag < 0){
+  } else if (decimalDigits < 0) {
     return new Error('Колличесво чисел после запятой может быть больше или равен нулю');
-  }else if (min >= max){
+  } else if (min >= max) {
     return new Error('Начальное значение диапазона не должно быть больше или равным конечному');
-  }else{
-    let rand = Math.random() * (max - min) + min;
-    return rand.toFixed(flag);
+  } else {
+    return (Math.random() * (max - min) + min).toFixed(decimalDigits);
   }
 }
-alert(randomDecimal(1, 9, 2));
+getRandomDecimal(1,5,2);
 
+
+//
