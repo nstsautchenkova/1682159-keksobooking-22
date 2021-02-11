e74d9e34dca027ad3c5a954633708e2ac3552ac6
nstsautchenkova <nst.sautchenkova@gmail.com>
2021-02-11 17:43:19 +0300

    Разделение на модули

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const NUMBER_IMG_AVATAR_MIN = 1;
+const NUMBER_IMG_AVATAR_MAX = 8;
+const LOCATION_X_MIN = 35.65000;
+const LOCATION_X_MAX = 35.70000;
+const LOCATION_X_DECIMAL_DIGITS = 5;
+const LOCATION_Y_MIN = 139.70000;
+const LOCATION_Y_MAX = 139.80000;
+const LOCATION_Y_DECIMAL_DIGITS = 5;
+const TITLE = 'Информацию об объявлении';
+const PRICE_MAX = 7;
+const ROOMS_MAX = 9;
+const QUESTS_MAX = 12;
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['2:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTION = 'Помещение большое, светлое, просторное. В нем трое больших окон. Справа от дверей стоят книжные шкафы. Их пять, и каждая заполнена книгами, журналами, газетами.';
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const ARRAY_DECLARATION_COUNT = 10;
+
+export {NUMBER_IMG_AVATAR_MIN, NUMBER_IMG_AVATAR_MAX, LOCATION_X_MIN, LOCATION_X_MAX, LOCATION_X_DECIMAL_DIGITS, LOCATION_Y_MIN, LOCATION_Y_MAX, LOCATION_Y_DECIMAL_DIGITS, TITLE, PRICE_MAX, ROOMS_MAX, QUESTS_MAX, TYPES, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTION, PHOTOS, ARRAY_DECLARATION_COUNT};
