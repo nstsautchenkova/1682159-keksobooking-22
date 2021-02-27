7ac99f613830c53dda98b13f80b454394a059532
nstsautchenkova <nst.sautchenkova@gmail.com>
2021-02-27 15:46:53 +0300

    Изменение в form.js

--- js/form.js
+++ js/form.js
@@ -1,6 +1,3 @@
-import { getRandomInteger } from './util.js';
-
-
 // Тип жилья - Цена за ночь, руб.
 const typeSelect = document.querySelector('#type');
 const getPriceMinValue = (typeSelectValue) => {
@@ -18,40 +15,27 @@ const getPriceMinValue = (typeSelectValue) => {
   }
 };
 
-const getPriceMin = () => {
-  typeSelect.addEventListener('change', () => {
-    const priceMin = getPriceMinValue(typeSelect.value);
 
-    const price = getRandomInteger(priceMin, 15000);
-    document.getElementById('price').placeholder = price;
-  });
-};
+typeSelect.addEventListener('change', () => {
+  const priceMin = getPriceMinValue(typeSelect.value);
+  document.getElementById('price').placeholder = priceMin;
+});
+
 
 // Время заезда и выезда
 let timeIn = document.querySelector('#timein');
-let timeInOption = timeIn.querySelector('option');
-let timeInValue = timeInOption.value;
 let timeOut = document.querySelector('#timeout');
-let timeOutOption = timeOut.querySelector('option');
-let timeOutValue = timeOutOption.value;
 
-const getTimeIn = () => {
-  timeIn.addEventListener('change', () => {
-    timeInValue = timeIn.value;
-    for (let i = 0; i < timeOut.length; i++) {
-      if (timeOut[i].value === timeInValue) timeOut[i].selected = true;
-    }
-  });
-};
 
-const getTimeOut = () => {
-  timeOut.addEventListener('change', () => {
-    timeOutValue = timeOut.value;
-    for (let i = 0; i < timeIn.length; i++) {
-      if (timeIn[i].value === timeOutValue) timeIn[i].selected = true;
-    }
-  });
-};
+timeIn.addEventListener('change', (event) => {
+  timeOut.value = event.target.value
+});
+
+
+timeOut.addEventListener('change', (event) => {
+  timeIn.value = event.target.value
+});
+
 
 // Количество комнат - Количество мест
 let numberRooms = document.querySelector('#room_number');
@@ -74,64 +58,60 @@ if (numberRooms.value == 1) {
   capacity[2].removeAttribute('disabled');
 }
 
-const getNumberRooms = () => {
-  numberRooms.addEventListener('change', () => {
-    for (let i = 0; i < numberRooms.length; i++) {
-      if (numberRooms.value === capacity[i].value) {
-        capacity[i].selected = true;
-      }
+
+numberRooms.addEventListener('change', () => {
+  for (let i = 0; i < numberRooms.length; i++) {
+    if (numberRooms.value === capacity[i].value) {
+      capacity[i].selected = true;
     }
-    if (numberRooms.value == 1) {
-      capacityDisabled();
-      capacityRemoveDisabled(2,2);
-    }
-    if (numberRooms.value == 2) {
-      capacityDisabled();
-      capacityRemoveDisabled(1,2);
-    }
-    if (numberRooms.value == 3) {
-      capacityDisabled();
-      capacityRemoveDisabled(0,2);
-    }
-    if (numberRooms.value == 100) {
-      capacityDisabled();
-      capacity[3].selected = true;
-      capacityRemoveDisabled(3,3);
-    }
-  });
-};
-
-
-const checkFormValidation = () => {
-  // validation #title
-  const MIN_TITLE_LENGTH = 30;
-  const MAX_TITLE_LENGTH = 100;
-
-  const titleInput = document.querySelector('#title');
-
-  titleInput.addEventListener('input', () => {
-    const valueLength = titleInput.value.length;
-
-    if (valueLength < MIN_TITLE_LENGTH) {
-      titleInput.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.');
-    } else if (valueLength > MAX_TITLE_LENGTH) {
-      titleInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) + ' симв.');
-    } else {
-      titleInput.setCustomValidity('');
-    }
-
-    titleInput.reportValidity();
-  });
-
-  // validation #price
-  const priceInput = document.querySelector('#price');
-  priceInput.addEventListener('input', () => {
-    priceInput.reportValidity();
-  });
-}
+  }
+  if (numberRooms.value == 1) {
+    capacityDisabled();
+    capacityRemoveDisabled(2, 2);
+  }
+  if (numberRooms.value == 2) {
+    capacityDisabled();
+    capacityRemoveDisabled(1, 2);
+  }
+  if (numberRooms.value == 3) {
+    capacityDisabled();
+    capacityRemoveDisabled(0, 2);
+  }
+  if (numberRooms.value == 100) {
+    capacityDisabled();
+    capacity[3].selected = true;
+    capacityRemoveDisabled(3, 3);
+  }
+});
 
 
 
-export { getPriceMin, getTimeIn, getTimeOut, getNumberRooms, checkFormValidation };
+
+// validation #title
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const titleInput = document.querySelector('#title');
+
+titleInput.addEventListener('input', () => {
+  const valueLength = titleInput.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) + ' симв.');
+  } else {
+    titleInput.setCustomValidity('');
+  }
+
+  titleInput.reportValidity();
+});
+
+// validation #price
+const priceInput = document.querySelector('#price');
+priceInput.addEventListener('input', () => {
+  priceInput.reportValidity();
+});
+
 
 
--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 import { createMap } from './map.js';
-import { getPriceMin, getTimeIn, getTimeOut, getNumberRooms, checkFormValidation } from './form.js';
+import './form.js';
 
-getPriceMin();
-getTimeIn();
-getTimeOut();
-getNumberRooms();
-checkFormValidation();
 createMap();
 
 
